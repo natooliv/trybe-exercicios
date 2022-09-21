8e0fba3a0b88d654ff6d3d8067d7c5de0f0b376a
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-09-21 15:32:14 -0300

    Atualização de aulas

---
+++ fundamentos/secao04-javascript/tiposprimitivos.js
@@ -0,0 +1,56 @@
+// Os tipos primitivos de referencia ou valores
+////let movie = "Avengers";// valor de string= sequencia de numeros, letras
+////let score = 10;// number literal , aceita numeros inteiros ou reais(QUEBRADOS)
+//////let isValid = true // valor booleano (true ou false);
+////////let name; // indefinida, não recebeu valor (undefined)
+////////let color = null; // recebe valor nulo, é util para começar um site com nenhuma cor e que a pessoa pode mudar a cor que quiser, pode receber uma reatribuição ou REDEFIÇÃO DE VALOR.
+//////////////
+
+// o js tem tipagem de variavel é dinamico , ele consegue indetificar o tipo de valor que está recebendo.
+// para saber qual tipo de variavel usa-se o typeof no console
+// os tipos de operadores +,-,*,/ **
+
+//let salary = 3500
+//console.log (salary + salary);
+//console.log(salary -salary);
+//console.log(salary * salary);
+//console.log(salary / salary)
+//console.log(3 ** 3);
+
+// o incremento ++(acrescentar 1)
+// decremento -- (retirar 1)
+//let salary = 3500
+//salary++;
+//console.log(salary)
+
+//let patientId = 50;
+//let isEnrolled = true;
+//const patientInfo = {
+//  firstName: 'Ana',
+ // lastName: 'Santos',
+//};
+//const patientEmail = 'ana@email.com';
+//console.log(typeof patientAge)
+
+// vai retornar undefined porque essa variavel não foi declarada
+
+//let patientId = '50';
+//let isEnrolled = true;//
+//const patientInfo =// {
+  //firstName: 'Ana',//
+  //lastName: 'Santos//',
+//};
+//const patientEmail = 'ana@email.com';
+//console.log( typeof patientId)
+// vai retornar string
+
+//const base = 5;
+//const heigth = 8;
+//const area =  base * heigth
+//console.log(area)
+const base = 5;
+const heigth = 8;
+const area =  base * heigth
+const perimeter = base + heigth + area
+console.log(perimeter)
+
