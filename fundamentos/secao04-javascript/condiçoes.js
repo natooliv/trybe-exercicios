fb202a8219d5b5979c0cc036ec6d8b26707fd1bf
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-09-21 18:17:43 -0300

    Atualização

---
+++ fundamentos/secao04-javascript/condiçoes.js
@@ -0,0 +1,36 @@
+// //condições
+// if - se    else -//senão
+
+//if(condicao){
+  //codigo -se acontecer essa condição, será executado faz teste logico
+//}
+//else if(outracondicao) {
+ // faz teste logico e pode ser usado quantas vezes for necessario
+//}
+//else {
+  // não faz teste logico, é usado somente se as outras condições voltaram com valor false
+//}
+
+//let trybe = 22;
+//if(trybe >=14 && trybe<14.40){
+ // console.log("Esquenta");
+//}
+//else if (trybe>=16.30 && trybe <17.50){
+ // console.log("Aula ao vivo");
+//}
+//else if(trybe >=19.40 && trybe < 17.50){
+  //console.log("Fechamento");
+//}
+//else{
+  //console.log("Fora dos momentos sincronos")
+//}
+const desafio = 60;
+if(desafio >= 80){
+  console.log("Parabens!");
+}
+else if (desafio <= 80 && desafio >= 60){
+  console.log("Voce está na lista de espera")
+}
+else{
+  console.log("Você foi reprovado!")
+}
--- fundamentos/secao04-javascript/tiposprimitivos.js
+++ fundamentos/secao04-javascript/tiposprimitivos.js
@@ -1,14 +1,13 @@
-// Os tipos primitivos de referencia ou valores
-////let movie = "Avengers";// valor de string= sequencia de numeros, letras
-////let score = 10;// number literal , aceita numeros inteiros ou reais(QUEBRADOS)
-//////let isValid = true // valor booleano (true ou false);
-////////let name; // indefinida, não recebeu valor (undefined)
-////////let color = null; // recebe valor nulo, é util para começar um site com nenhuma cor e que a pessoa pode mudar a cor que quiser, pode receber uma reatribuição ou REDEFIÇÃO DE VALOR.
-//////////////
+// Os tipos primitivos(raiz) de referencia ou valores
+//let movie = "Avengers";// valor de string= sequencia de numeros, letras
+//let score = 10;// number literal , aceita numeros inteiros ou reais(QUEBRADOS)
+//let isValid = true // valor booleano (true ou false);
+//let color = null; // recebe valor nulo, é util para começar um site com nenhuma cor e que a pessoa pode mudar a cor que quiser, pode receber uma reatribuição ou REDEFIÇÃO DE VALOR.
+//let wow; undefined está indefinida 
 
-// o js tem tipagem de variavel é dinamico , ele consegue indetificar o tipo de valor que está recebendo.
-// para saber qual tipo de variavel usa-se o typeof no console
-// os tipos de operadores +,-,*,/ **
+// o js tem tipagem  ou fraca de variavel é dinamico , ele consegue indetificar o tipo de valor que está recebendo.
+// para saber qual tipo de variavel usa-se o TYPEOF no console
+// os tipos de operadores +,-,*,/ ** 
 
 //let salary = 3500
 //console.log (salary + salary);
@@ -50,7 +49,7 @@
 //console.log(area)
 const base = 5;
 const heigth = 8;
-const area =  base * heigth
-const perimeter = base + heigth + area
+const area =  base * heigth;
+const perimeter = base + heigth + area;
 console.log(perimeter)
 
