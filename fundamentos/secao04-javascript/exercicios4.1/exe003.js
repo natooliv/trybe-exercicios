eb3ec81ab6f728f9f4cd3e0f697a965ee261336c
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-09-24 08:34:44 -0300

    Atualização

--- fundamentos/secao04-javascript/exercicios4.1/exe003.js
+++ fundamentos/secao04-javascript/exercicios4.1/exe003.js
@@ -1,7 +1,39 @@
-let fatorial = 1;
+//let fatorial = 1;
+//
+//f//or (let index = 10; index > 0; index -= 1) {
+  //fatorial *= index;
+//}
 
-for (let index = 10; index > 0; index -= 1) {
-  fatorial *= index;
+//console.log(fatorial);
+
+// calculando fatorial
+//-----------------------------------------------------------------------
+
+
+//let palavra = "trybe";
+//let reverso ="";
+/// reverso += palavra[palavra.length -1 - index];
+//}
+//console.log(reverso)
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
 }
 
-console.log(fatorial);
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
+
+
