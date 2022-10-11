b0e58a4f50e525ce168a66497400d5cb3abd4573
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-10-11 15:19:24 -0300

    exercicios

---
+++ fundamentos/bootstrap/bootstrap-5.2.2-dist/script.js
@@ -0,0 +1,7 @@
+var field = document.getElementById('datepicker');
+var picker = new Pikaday({
+    onSelect: function(date) {
+        field.value = picker.toString();
+    }
+});
+field.parentNode.insertBefore(picker.el, field.nextSibling);
