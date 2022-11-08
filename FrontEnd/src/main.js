1190286689a848b28eba4e84ac106c8cd11cd6c2
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-08 19:23:08 -0300

    Gerador de SEnhas

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,6 +1,7 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import copy from 'clipboard-copy';
-import "./style.css";
+
+import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -8,8 +9,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
 
-});displayPasswordEl.addEventListener('click', async (event) => {
+displayPasswordEl.addEventListener('click', async (event) => {
   await copy(event.target.innerHTML);
   alert('Senha copiada!');
 });
