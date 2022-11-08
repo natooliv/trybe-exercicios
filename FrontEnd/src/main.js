eed5d0e8a97a27164da7bca3ad63a154ffd55051
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-08 19:00:10 -0300

    ClipBoard

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,5 +1,15 @@
 import { nanoid } from 'nanoid'
+import copy from 'clipboard-copy';
 import "./style.css";
 
-const randomPassword = nanoid()
-console.log(randomPassword)
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+
+});displayPasswordEl.addEventListener('click', async (event) => {
+  await copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
